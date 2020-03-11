3168bd5aac9a636a7d490fc8fccaa78e47e855da
imadham <imad.hamameh@gmail.com>
2020-03-11 07:06:52 +0300

    adding Book controller

---
+++ src/main/java/imad/imad/imadWeb/controller/BookController.java
@@ -0,0 +1,21 @@
+package imad.imad.imadWeb.controller;
+
+import imad.imad.imadWeb.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+    model.addAttribute("Books",bookRepository.findAll());
+    return "books";
+    }
+}
